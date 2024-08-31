b55a8cddc875620a06d1140bd6ddb7e97756c8a6
Phil Stubbs <philipstubbs13@gmail.com>
2024-08-31 09:36:30 -0500

    nextjs portfolio site

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Intro from '@/components/intro'
+import { MDXRemote } from 'next-mdx-remote/rsc'
+
+export default function Home() {
+  const content = `
+    # This is a markdown heading
+  `
+  return (
+    <section className="py-24">
+      <div className="container max-w-3xl">
+        <Intro />
+
+        <MDXRemote source={content} />
+      </div>
+    </section>
+  )
+}
---
+++ app/posts/page.tsx
@@ -0,0 +1,17 @@
+import { getPosts } from '@/lib/posts'
+// import PostsWithSearch from '@/components/posts-with-search'
+import Posts from '@/components/posts'
+
+export default async function PostsPage() {
+  const posts = await getPosts()
+
+  return (
+    <section className="pb-24 pt-40">
+      <div className="container max-w-3xl">
+        <h1 className="title mb-12">Posts</h1>
+
+        <Posts posts={posts} />
+      </div>
+    </section>
+  )
+}
